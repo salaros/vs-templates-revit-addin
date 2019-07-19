8f2e8a8e1724549d9224407546617acf88f9c24d
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2019-07-19 08:12:46 +0100

    adding item templates

--- Item.ExternalCommand/ExternalCommand.cs
+++ Item.ExternalCommand/ExternalCommand.cs
@@ -1,16 +1,20 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
-namespace $safeprojectname$
+namespace $rootnamespace$
 {
-    /// <summary>
+	/// <summary>
     /// A sample command.
     /// </summary>
     /// <seealso cref="T:Autodesk.Revit.UI.IExternalCommand" />
     [Transaction(TransactionMode.Manual)]
     [Regeneration(RegenerationOption.Manual)]
-    public class HelloWorldCommand : IExternalCommand
+    public class $safeitemrootname$ : IExternalCommand
     {
         /// <summary>
         /// Executes the specified Revit command <see cref="ExternalCommand"/>.
@@ -24,7 +28,8 @@ namespace $safeprojectname$
             ExternalCommandData commandData,
             ref string message,
             ElementSet elements
-        ){
+        )
+        {
             TaskDialog.Show("It's alive!", "Hello World!");
             return Result.Succeeded;
         }
